894facff9ef0986b05ed373bfd2f3df389b45459
mrdulin <novaline.dulin@gmail.com>
2020-08-04 18:51:16 +0800

    disable allowJs option for tsc

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./jest.setup.js'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
