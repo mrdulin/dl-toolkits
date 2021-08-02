bc5dbfd9dbcbb45b2d4f1367e44fba5d6e574e5d
mrdulin <novaline.dulin@gmail.com>
2021-08-02 19:33:40 +0800

    feat: add utility types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,18 @@
-type DecoratorFunction<T> = (target: any, propertyKey: string, decriptor: T) => T;
+export type DecoratorFunction<T> = (target: any, propertyKey: string, decriptor: T) => T;
 
-interface IkeyOfObject {
+export interface IkeyOfObject {
   [propName: string]: any;
 }
 
-type ClassType<T> = new () => T;
+export type ClassType<T> = new () => T;
 
 /**
  * pick method signature type from an interface
  */
-type PickMethod<T, MethodName extends keyof T> = T[MethodName] extends (...args: any[]) => any
+export type PickMethod<T, MethodName extends keyof T> = T[MethodName] extends (...args: any[]) => any
   ? (...args: Parameters<T[MethodName]>) => ReturnType<T[MethodName]>
   : never;
 
-export { DecoratorFunction, IkeyOfObject, PickMethod, ClassType };
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type MakeRequired<T extends object, Prop extends keyof T> = Omit<T, Prop> & Record<Prop, Required<T[Prop]>>;
