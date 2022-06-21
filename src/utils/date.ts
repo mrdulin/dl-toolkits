0fd859b02c6a3c1f39acb04c0b94e6f5281e68ba
mrdulin <novaline.dulin@gmail.com>
2022-06-21 14:08:14 +0800

    chore: generate comments

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,8 @@
+/**
+ * Convert a date string to a date string in the format dd/mm/yyyy
+ * @param {string} s - string
+ * @returns The date in the format dd/mm/yyyy
+ */
 function ddmmyyyy(s: string) {
   if (!isValidDate(s)) {
     throw new Error('invalid date string');
@@ -11,6 +16,22 @@ function ddmmyyyy(s: string) {
   return d + '/' + m + '/' + y;
 }
 
+/**
+ * "Check if the string is a valid date."
+ *
+ * Here's a longer summary: "Check if the string is a valid date. A valid date is a string that can be
+ * parsed into a JavaScript Date object."
+ *
+ * Here's a longer summary with more detail: "Check if the string is a valid date. A valid date is a
+ * string that can be parsed into a JavaScript Date object. If the string is a valid date, return true.
+ * Otherwise, return false."
+ *
+ * Here's a longer summary with more detail: "Check if the string is a valid date. A valid date is a
+ * string that can be parsed into a JavaScript Date object. If the string is a valid date, return true.
+ * Otherwise, return false
+ * @param {string} s - string
+ * @returns A boolean value.
+ */
 function isValidDate(s: string): boolean {
   const timestamp = Date.parse(s);
   return !Number.isNaN(timestamp);
